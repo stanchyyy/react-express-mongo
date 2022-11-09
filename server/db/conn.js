66193519c1698fb1a8a0266c8ecfad526229b6b5
stanislavatanasov <stanchyyy@gmail.com>
2022-11-10 00:32:03 +0200

    server and conn works

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,7 +1,26 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
-const db = process.env.DB_CONNECTION;
+const dbConnection = process.env.DB_URI;
+const client = new MongoClient(dbConnection,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
+let db;
 
+    export async function  connectToDB () {
+    await client.connect().then((value)=>{
+      db = value.db("pizzas");
+      console.log("Connected successfully to BD.");
+    },(err)=>{
+      console.log(err);
+    });
 
-export default dbConn;
+  }
+
+  export function getDb (){
+    return db;
+  }
+
--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import {MongoClient} from "mongodb";
+
+import {connectToDB, getDb} from "./db/conn.js"
 
 //import ingredientsRoute from "./routes/ingredients";
-//import dbConn from "./db/conn";
 
-//can this be converted to es6?
 dotenv.config({ path: "./config.env" });
 const port = process.env.PORT;
-
+const uri = process.env.DB_URI;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 //app.use(ingredientsRoute());
-//const dbo = dbconn;
+
+
 app.listen(port,()=>{
-    app.get("/api/ingredients",(req,res)=>{
-        res.send("reached not implemented db route")
+    connectToDB((err)=>{
+        if(err) console.log(err);
     })
+
     console.log(`Server is listening on port ${port}`);
 })
