28af1db3e36602f76df46b089b91f76a1d44ed75
stanislavatanasov <stanchyyy@gmail.com>
2022-11-18 03:16:31 +0200

    added patch and delete queries

---
+++ server/models/ingredientsModel.js
@@ -0,0 +1,9 @@
+export class Ingredient{
+    constructor(name,layer,saltLevel,vegan,spicy){
+        this.name = name;
+        this.layer = layer;
+        this.saltLevel = saltLevel;
+        this.vegan =vegan;
+        this.spicy = spicy;
+    }
+}
--- server/routes/ingredients.js
+++ server/routes/ingredients.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {getDb} from "../db/conn.js"
 import { ObjectId } from "mongodb";
-
+import {Ingredient} from "../models/ingredientsModel.js";
 
 
 export const ingredientsRoutes = express.Router();
@@ -20,6 +20,7 @@ ingredientsRoutes.route("/api/ingredients").get(function(req,res){
             console.log(result)
         })
 })
+// This section will help you get a single ingredient by name.
 
 ingredientsRoutes.route("/api/ingredients/:name").get((req,res)=>{
     console.log("reached find by name")
@@ -27,5 +28,54 @@ ingredientsRoutes.route("/api/ingredients/:name").get((req,res)=>{
     let nameQuery = {name: req.params.name};
     db_connect.collection(collection).findOne(nameQuery,(err,result)=>{
         res.json(result);
+    })
+})
+// This section will help you add ingredient.
+
+ingredientsRoutes.route("/api/ingredients/add").post((req,res)=>{
+    console.log("reached insert ingredient")
+    let db_connect = getDb();
+    let ingredientObject = new Ingredient(
+        req.body.name,
+        req.body.layer,
+        req.body.saltLevel,
+        req.body.vegan,
+        req.body.spicy
+    );
+    if(Object.keys(req.body).length>0){
+    db_connect.collection(collection).insertOne(ingredientObject,(error,result)=>{
+        res.json(result);
+    })}else{
+        res.status(400).send({error: "All fields require values"})
+    }
+})
+
+// This section will help you update a single ingredient by name.
+
+ingredientsRoutes.route("/api/ingredients/update/:name").patch((req,res)=>{
+    let db_connect = getDb();
+    let nameQuery = {name: req.params.name};
+    let newValue = {
+        $set: new Ingredient(
+            req.body.name,
+            req.body.layer,
+            req.body.saltLevel,
+            req.body.vegan,
+            req.body.spicy
+        )
+    }
+    db_connect.collection(collection).updateOne(nameQuery,newValue,function(err,response){
+        res.json(response);
+    })
+
+})
+
+// This section will help you delete a record
+
+ingredientsRoutes.route("/api/ingredients/delete/:name").delete((req,res)=>{
+    let db_connect = getDb();
+    let nameQuery = {name: req.params.name};
+    db_connect.collection(collection).deleteOne(nameQuery,(error,response)=>{
+        res.json(response);
     })
 })
