ffa4a2995423f2ca42a0001bda5015c3d52c8699
stanislavatanasov <stanchyyy@gmail.com>
2022-12-02 02:40:33 +0200

    added table with ingredients

--- client/src/components/Products/list-ingredients.js
+++ client/src/components/Products/list-ingredients.js
@@ -1,20 +1,38 @@
 import Container from "react-bootstrap/Container"
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-
+import Table from 'react-bootstrap/Table';
 import Text from "../../local-json/products-text.json"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function RecordList(){
+
+}
 
 function ListIngredients(){
     return(
         <Container>
             <Card className="text-center">
-                <Card.Img variant="top" src="../img/ingredients.jpg"/>
+            <Card.Body>
                 <Card.Title>{Text.heading}</Card.Title>
-
+                <Card.Text>{Text.text}</Card.Text>
+                <h2>Dough</h2>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Layer</th>
+                            <th>Salt Level</th>
+                            <th>Spicy</th>
+                            <th>Vegan</th>
+                            <th>Type</th>
+                        </tr>
+                    </thead>
+                    <tbody>{RecordList()}</tbody>
+                </Table>
+                </Card.Body>
             </Card>
         </Container>
     )
 }
-
 export default ListIngredients;
