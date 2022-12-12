05ff81de5c8d556a28a6227a0bddb61d00d0e398
stanislavatanasov <stanchyyy@gmail.com>
2022-12-12 10:58:56 +0200

    can't make async function work in react. will switch to useEffect

--- client/src/components/Products/list-ingredients.js
+++ client/src/components/Products/list-ingredients.js
@@ -3,69 +3,43 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Text from "../../local-json/products-text.json"
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-const ingredient = (ingredient)=>{
-    (
-        <Row xs={1} md={4} className="g-4">
-            <Col>
-                <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                    <Card.Title>{ingredient.name}</Card.Title>
-                    <Card.Text>{ingredient.layer}</Card.Text>
-                    <Card.Text>{ingredient.saltLevel}</Card.Text>
-                    <Card.Text>{ingredient.Vegan}</Card.Text>
-                    <Card.Text>{ingredient.type}</Card.Text>
-                    <Card.Text>{ingredient.spicy}</Card.Text>
-                    </Card.Body>
-                </Card>
-        </Col>
-    </Row>
-    )};
 
-function ListIngredients(){
 
-    const [ingredients, setIngredients] = useState([]);
-     // This method fetches the records from the database.
-    useEffect(()=>{
-        async function getIngredients(){
-            const response = await fetch("http://localhost:5001/api/ingredients");
-            const ingredients = await response.json();
-            console.log(ingredients);
-            setIngredients(ingredients);
-            console.log(ingredients);
-        }
-        getIngredients();
-        return;
-    }, [ingredient.length]);
+async function ListIngredients() {
 
-    return(
+    const response = await fetch("http://localhost:5001/api/ingredients");
+    const data = await response.json();
+    data.then((ingredients)=>
+        ingredients.map((ingredient) =>
+            <Row xs={1} md={4} className="g-4">
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                            <Card.Title>{ingredient.name}</Card.Title>
+                            <Card.Text>{ingredient.layer}</Card.Text>
+                            <Card.Text>{ingredient.saltLevel}</Card.Text>
+                            <Card.Text>{ingredient.vegan}</Card.Text>
+                            <Card.Text>{ingredient.type}</Card.Text>
+                            <Card.Text>{ingredient.spicy}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+    ));
+
+
+    return (
         <Container>
             <Card className="text-center">
-            <Card.Body>
-            <Card.Title>{Text.heading}</Card.Title>
-            <Card.Text>{Text.text}</Card.Text>
-                <h2>Dough</h2>
-                <Row xs={1} md={4} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-            </Card.Body>
+                <Card.Body>
+                    <Card.Title>{Text.heading}</Card.Title>
+                    <Card.Text>{Text.text}</Card.Text>
+                    <h2>Dough</h2>
+                    {data}
+                </Card.Body>
             </Card>
         </Container>
     )
